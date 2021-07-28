fb7d1e1ebf03198eaa42f2108a7c2c9cd0ac9203
giwilorjelly <gwlgidwani@gmail.com>
2021-07-29 00:05:36 +0530

    modified yfinance api calling function

--- fetch.py
+++ fetch.py
@@ -2,21 +2,27 @@ import yfinance as yf
 import datetime
 
 
-def get_data_yf(ticker):
-    print("fetching data for", ticker)
-    # getting current date
-    info = yf.Ticker(ticker).info
-    info_keys = info.keys()
-    try:
-        data = dict()
-        data['ticker'] = ticker
-        data['previousClose'] = info['previousClose']
-        data['currency'] = info['currency']
-        data['fetchTime'] = datetime.datetime.utcnow()
-        data['longName'] = info['longName'] if 'longName' in info_keys else data['shortname']
-        data['market'] = info['market'] if 'market' in info_keys else 'unavailable'
-        data['quoteType'] = info['quoteType'] if 'quoteType' in info_keys else 'unavailable'
+def get_data_yf(tickers):
+    api_res = yf.Tickers(tickers).tickers
+    tickers = tickers.upper().split()
+    datalist = []
 
-    except Exception as e:
-        return False
-    return data
+    for ticker in tickers:
+        info = api_res[ticker].info
+        info_keys = info.keys()
+        try:
+            data = dict()
+            data['ticker'] = ticker
+            data['previousClose'] = info['previousClose']
+            data['currency'] = info['currency']
+            data['fetchTime'] = datetime.datetime.utcnow()
+            data['longName'] = info['longName'] if 'longName' in info_keys else info['shortName']
+            data['market'] = info['market'] if 'market' in info_keys else 'unavailable'
+            data['quoteType'] = info['quoteType'] if 'quoteType' in info_keys else 'unavailable'
+            datalist.append(data)
+
+        except Exception as e:
+            print(str(e))
+            return False
+
+    return datalist
